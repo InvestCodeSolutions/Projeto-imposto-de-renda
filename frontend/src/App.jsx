a67a1bc112b2f40297956f9ac5e706d9619a7c78
InvestCodeSolutions <onlyupinvest1906@gmail.com>
2025-06-02 22:37:07 -0300

    Fix Frontend Setup Errors

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>DeclaraJá</div>} />
+    </Routes>
+  );
+}
+
+export default App;
